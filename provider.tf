132b99e207a5b6d6634394f41cf513f26ee16901
hkantare <hkantare@in.ibm.com>
2022-04-20 10:42:26 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">0.12.0"
+}
+
+provider "ibm" {
+  version = "~>1.38.0"
+  # Configuration options
+}
+
---
+++ variables.tf
@@ -0,0 +1,12 @@
+/*variable "ibmcloud_api_key" {
+  description = "IBM Cloud Platform API Key"
+}*/
+
+variable "resource_group_name" {
+  description = "Where to create resources"
+  default = "Default"
+}
+
+variable "vpc_name" {
+  default = "harini-test-vpc"
+}
